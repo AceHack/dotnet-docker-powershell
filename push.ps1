8927781a127c76a68105806e17509c69c204974b
astainba <aaron.stainback@itron.com>
2018-01-28 04:12:07 -0500

    Adding more OSs

--- push.ps1
+++ push.ps1
@@ -4,4 +4,8 @@ $ErrorActionPreference = 'Stop';
 "$env:DOCKER_PASS" | docker login --username "$env:DOCKER_USER" --password-stdin
 
 # push images
-docker push acehack/dotnet:2.0.5-sdk-powershell
+docker push acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-1709
+
+# rebase for windows 1709
+# npm install -g rebase-docker-image
+# rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-1709 -b microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-1709
