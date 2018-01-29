bd86a5b927da36ab90d5dd9b0659e92eb3ac4ead
astainba <aaron.stainback@itron.com>
2018-01-28 21:32:49 -0500

    fixing current branch

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -9,5 +9,5 @@ docker push acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016
 
 # rebase for windows 1709
 npm install -g rebase-docker-image
-rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-rebase-2.1.4-nanoserver-1709 -b microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-1709
+rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-rebase-2.1.4-nanoserver-1709 -b microsoft/nanoserver:1709
 #rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-rebase-2.1.4-nanoserver-1709 -s microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-sac2016 -b microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-1709
