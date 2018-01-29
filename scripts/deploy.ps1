8b182c063c9096359ceb531c278c40ffec4847ba
astainba <aaron.stainback@itron.com>
2018-01-28 22:00:11 -0500

    fixing path issues

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -13,9 +13,9 @@ rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016
 #rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-2.1.4-rebase-nanoserver-1709 -s microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-sac2016 -b microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-1709
 
 # push multi-arch tags
-..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.0.5-rebase.yml
-..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.0-rebase.yml
-..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2-rebase.yml
-#..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.0.5.yml
-#..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.0.yml
-#..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.yml
+.\tools\manifest-tool-windows-amd64.exe push from-spec dotnet\2.0\sdk-powershell-2.0.5-rebase.yml
+.\tools\manifest-tool-windows-amd64.exe push from-spec dotnet\2.0\sdk-powershell-2.0-rebase.yml
+.\tools\manifest-tool-windows-amd64.exe push from-spec dotnet\2.0\sdk-powershell-2-rebase.yml
+#.\tools\manifest-tool-windows-amd64.exe push from-spec dotnet\2.0\sdk-powershell-2.0.5.yml
+#.\tools\manifest-tool-windows-amd64.exe push from-spec dotnet\2.0\sdk-powershell-2.0.yml
+#.\tools\manifest-tool-windows-amd64.exe push from-spec dotnet\2.0\sdk-powershell-2.yml
