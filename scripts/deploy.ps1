06fbe3486a49f8812bb4e41de2fa668ceed6a3bf
astainba <aaron.stainback@itron.com>
2018-01-28 21:53:26 -0500

    adding multi-arch to build

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -9,5 +9,13 @@ docker push acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016
 
 # rebase for windows 1709
 npm install -g rebase-docker-image
-rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-rebase-2.1.4-nanoserver-1709 -b microsoft/nanoserver:1709
-#rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-rebase-2.1.4-nanoserver-1709 -s microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-sac2016 -b microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-1709
+rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-2.1.4-rebase-nanoserver-1709 -b microsoft/nanoserver:1709
+#rebase-docker-image acehack/dotnet:2.0.5-sdk-powershell-2.1.4-nanoserver-sac2016 -t acehack/dotnet:2.0.5-sdk-powershell-2.1.4-rebase-nanoserver-1709 -s microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-sac2016 -b microsoft/dotnet:2.0.5-sdk-2.1.4-nanoserver-1709
+
+# push multi-arch tags
+..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.0.5-rebase.yml
+..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.0-rebase.yml
+..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2-rebase.yml
+#..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.0.5.yml
+#..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.0.yml
+#..\tools\manifest-tool-windows-amd64.exe push from-spec ..\dotnet\2.0\sdk-powershell-2.yml
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,7 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     docker login --username "$DOCKER_USER" --password "$DOCKER_PASS";
     docker push acehack/dotnet:2.0.5-sdk-powershell-2.1.4-stretch;
+    ../tools/manifest-tool-linux-amd64 push from-spec ../dotnet/2.0/sdk-powershell-2.0.5.yml
+    ../tools/manifest-tool-linux-amd64 push from-spec ../dotnet/2.0/sdk-powershell-2.0.yml
+    ../tools/manifest-tool-linux-amd64 push from-spec ../dotnet/2.0/sdk-powershell-2.yml
 fi;
